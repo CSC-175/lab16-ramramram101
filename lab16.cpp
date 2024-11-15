223c93e26708d76cfecc41e6356378f1c512cb31
Noah Mahnke <theshine@umich.edu>
2024-11-14 20:14:44 -0500

    Update lab16.cpp

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,8 @@
-// Code to implement the gcf function goes here
+// C#include <iostream>
+using namespace std;
+int gcf(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcf(b, a % b);
+}
