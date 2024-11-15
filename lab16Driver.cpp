dab84c5cef3e7448e016f23f55a336989eae9980
Noah Mahnke <theshine@umich.edu>
2024-11-14 20:15:27 -0500

    Update lab16Driver.cpp

--- lab16Driver.cpp
+++ lab16Driver.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include "./lab16.cpp"
 using namespace std;
-
-int main()
-{
-    int a, b;
-
-    cout <<" Enter two integer values you want to find the GCF of: ";
-    cin >> a >> b;
-    cout << "The greatest common factor of " << a  <<" and " << b << " is " << gcf(a,b)<< endl;
+int gcf (int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcf (b, a % b);
 }
